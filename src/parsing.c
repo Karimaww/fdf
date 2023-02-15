67e69886550f27f61f5cc7096d9f459ccd9374ca
Karima <karimasadykova@gmail.com>
2023-02-15 14:45:30 +0100

    map functions modifie

--- src/parsing.c
+++ src/parsing.c
@@ -1,5 +1,6 @@
 #include "../include/parser.h"
 
+void	print_map_me(t_point **map, int x, int y);
 t_point	**copy_map(t_point **src, t_point **dest, int x, int y)
 {
 	int i;
@@ -9,19 +10,16 @@ t_point	**copy_map(t_point **src, t_point **dest, int x, int y)
 	j = 0;
 	if (!src || !dest)
 		return (NULL);
-	while (src[i] && i < y)
+	while (src && i < y && src[i])
 	{
-		while (j < x)
+		while (dest && j < x && dest[i])
 		{
-			printf("ay\n");
 			dest[i][j].z = src[i][j].z;
 			dest[i][j].color = src[i][j].color;
-			printf("ay\n");
 			j++;
 		}
 		j = 0;
 		i++;
-		printf("oy\n");
 	}
 	return (dest);
 }
@@ -49,11 +47,12 @@ void	print_map_me(t_point **map, int x, int y)
 	{
 		while (j < x)
 		{
-			printf("map[i][j].z : %d\nmap[i][j].color : %d\n", map[i][j].z,map[i][j].color);
+			printf("\t%d %d", map[i][j].z,map[i][j].color);
 			j++;
 		}
 		j = 0;
 		i++;
+		printf("\n");
 	}
 	return;
 }
@@ -61,11 +60,9 @@ void	print_map_me(t_point **map, int x, int y)
 t_point **init_point_map(t_point **map, int sizex, int sizey)
 {
 	int	i;
-	int j;
 	t_point	**new_map;
 
 	i = 0;
-	j = 0;
 	new_map = (t_point **) malloc(sizeof(t_point *) * sizey);
 	if (!new_map)
 		return (NULL);
@@ -74,23 +71,12 @@ t_point **init_point_map(t_point **map, int sizex, int sizey)
 		new_map[i] = (t_point *) malloc(sizeof(t_point) * sizex);
 		if (!new_map[i])
 			return (NULL);
-		while (new_map[i] && j < sizex)
-		{
-			new_map[i][j].z = 0;
-			new_map[i][j].color = 0;
-			j++;
-		}
-		j = 0;
 		i++;
 	}
-	if (map == NULL)
-	{
-		printf("here??\n");
-		new_map = copy_map(map, new_map, sizex, sizey);
-		free_map(map);
-	}
-	printf("here: \n");
-	//print_map_me(new_map, sizex, sizey);
+	if (!map)
+		return (new_map);
+	copy_map(map, new_map, sizex, sizey);
+	free(map);
 	return (new_map);
 }
 
@@ -113,30 +99,49 @@ void	fill_map(t_map *map, char *line, int x, int y)
 
 	i = 0;
 	full = ft_split(line, ' ');
-	//point = malloc(sizeof(t_point));
 	map->map = init_point_map(map->map, x, y);
+	print_map_me(map->map, map->sizex, map->sizey);
+	printf("---------------------------------------------\n");
 	while (full && full[i] && i < x)
 	{
+		printf("full[%d] : %s\n", i, full[i]);
 		sep = ft_split(full[i], ',');
-		point.z = ft_atoi(full[0]);
+		point.z = ft_atoi(sep[0]);
 		if (sep[1])
 			point.color = hex_to_trgb(sep[1] + 3);
 		else
 			point.color = 0;
 		map->map[y - 1][i] = point;
+		printf("map->map[%d][%d] : %d\n", y - 1, i, map->map[y - 1][i].z);
 		i++;
 	}
+}
+
+void	free_split(char **str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
 }
 
 void	read_map(t_map	*map, int f)
 {
 	char	*line;
+	char	**tmp;
 
 	line = get_next_line(f);
+	tmp = ft_split(line, ' ');
+	map->sizex = len_tab(tmp);
+	free_split(tmp);
 	map->sizey = 0;
 	while (line)
 	{
-		map->sizex = len_tab(ft_split(line, ' '));
 		map->sizey += 1;
 		fill_map(map, line, map->sizex, map->sizey);
 		line = get_next_line(f);
@@ -152,6 +157,9 @@ t_map	*parser(const char *file)
 	if (!f)
 		return (write(2, "Error\n", 6), NULL);
 	map = (t_map*)malloc(sizeof(t_map));
+	map->map = NULL;
+	map->sizex = 0;
+	map->sizey = 0;
 	read_map(map, f);
 	close(f);
 	return (map);
